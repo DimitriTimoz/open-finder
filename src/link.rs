c1ed2fb9ba81abff35b2a4b5dc4edf37f79ddf14
DimitriTimoz <45998396+DimitriTimoz@users.noreply.github.com>
2022-10-10 00:40:09 +0200

    find links

--- src/link.rs
+++ src/link.rs
@@ -2,23 +2,23 @@ use std::fmt::Debug;
 
 use self::errors::UrlError;
 
-pub(crate) struct Url {
+pub struct Url {
     url: String,
     host: String,
 }
 
 impl Url {
-    fn from (v: &dyn ToString) -> Result<Self, UrlError> {
+    pub fn parse(v: &dyn ToString) -> Result<Self, UrlError> {
         use UrlError::*;
 
         let url = v.to_string();
-        let mut url_split = url.split("://"); 
+        let mut url_split = url.split("://");
         match url_split.next() {
             Some(v) => {
-                if v.is_empty(){
+                if v.is_empty() {
                     return Err(NoProtocol);
                 }
-            },
+            }
             None => return Err(NotValidUrl),
         };
 
@@ -28,12 +28,18 @@ impl Url {
         };
         Ok(Url {
             url: url.to_string(),
-            host, 
+            host,
         })
     }
     /// Get the protocol of the url before the `://`
     fn get_protocol(&self) -> &str {
         self.url.split("://").next().unwrap()
+    }
+}
+
+impl ToString for Url {
+    fn to_string(&self) -> String {
+        self.url.to_string()
     }
 }
 
@@ -51,7 +57,34 @@ mod errors {
         NotValidUrl,
         NoProtocol,
     }
+}
 
+pub fn get_links(content: &str) -> Vec<Url> {
+    let mut links = Vec::new();
+    if let Some(i) = content.find("://") {
+        let mut start = i;
+        let content_p = content.as_bytes();
+        let mut end = content.len();
+        for j in (0..i).rev() {
+            if content_p[j].is_ascii_alphabetic() {
+                start = j;
+            } else {
+                break;
+            }
+        }
+        for j in i..end {
+            if content_p[j] == b' ' || content_p[j] == b'"' || content_p[j] == b'\'' {
+                end = j;
+                break;
+            } else {
+                end = j;
+            }
+        }
+        let link = Url::parse(&content[start..end].to_string()).unwrap();
+        links.push(link);
+        links.extend(get_links(&content[end..]));
+    }
+    links
 }
 
 #[cfg(test)]
@@ -60,21 +93,34 @@ mod tests {
 
     #[test]
     fn test_url() {
-        let url = Url::from(&"https://www.google.com").unwrap();
-    
+        let url = Url::parse(&"https://www.google.com").unwrap();
+
         assert_eq!(url.url, "https://www.google.com");
         assert_eq!(url.host, "www.google.com");
 
-        let url = Url::from(&"https://www.youtube.com/watch?v=dQw4w9WgXcQ").unwrap();
+        let url = Url::parse(&"https://www.youtube.com/watch?v=dQw4w9WgXcQ").unwrap();
         assert_eq!(url.host, "www.youtube.com");
         assert_eq!(url.get_protocol(), "https");
 
-        assert!(Url::from(&"www.google.com").is_err());
-        assert!(Url::from(&"http:/www.google.com").is_err());
-        assert!(Url::from(&"http://www.rust-lang.org/").is_ok());
-    
+        assert!(Url::parse(&"www.google.com").is_err());
+        assert!(Url::parse(&"http:/www.google.com").is_err());
+        assert!(Url::parse(&"http://www.rust-lang.org/").is_ok());
 
-        assert!(Url::from(&"://www.rust-lang.org/").is_err());
+        assert!(Url::parse(&"://www.rust-lang.org/").is_err());
+    }
 
+    #[test]
+    fn test_get_links() {
+        let content = r#"<a href="https://www.google.com">Google</a>"#;
+        let links = get_links(content);
+        assert_eq!(links.len(), 1);
+        assert_eq!(links[0].to_string(), "https://www.google.com");
+
+        // Check mutliple links
+        let content = r#"<a href="https://www.google.com">Google</a><a href="https://www.youtube.com">Youtube</a>"#;
+        let links = get_links(content);
+        assert_eq!(links.len(), 2);
+        assert_eq!(links[0].to_string(), "https://www.google.com");
+        assert_eq!(links[1].to_string(), "https://www.youtube.com");
     }
 }
